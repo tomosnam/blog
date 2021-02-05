afde476ba65cc2546699093a4ef19fe0224777cb
tomosnam <nmtomas29@gamil.com>
2021-02-05 03:04:02 -0600

    refactoring folder structure

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import useFetch from '../hooks/useFetch'
+import BlogList from './BlogList'
+
+const Home = () => {
+  const { data: blogs, isPending, error } = useFetch(' http://localhost:8080/blogs')
+
+  return (
+    <div className='home'>
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title='All Blogs!' />}
+    </div>
+  )
+}
+
+export default Home
