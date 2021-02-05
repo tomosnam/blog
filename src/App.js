b2f0e63aa664c2b2279adbc3f13dbe3645299620
tomosnam <nmtomas29@gamil.com>
2021-02-05 03:02:13 -0600

    refactoring folder structure

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import Navbar from './Navbar'
-import Home from './Home'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Create from './Create'
-import BlogDetails from './BlogDetails'
-import NotFound from './NotFound'
+import Create from './components/Create'
+import BlogDetails from './components/BlogDetails'
+import NotFound from './components/NotFound'
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className='App'>
         <Navbar />
-        <div className="content">
+        <div className='content'>
           <Switch>
-            <Route exact path="/">
+            <Route exact path='/'>
               <Home />
             </Route>
-            <Route path="/create">
+            <Route path='/create'>
               <Create />
             </Route>
-            <Route path="/blogs/:id">
+            <Route path='/blogs/:id'>
               <BlogDetails />
             </Route>
-            <Route path="*">
+            <Route path='*'>
               <NotFound />
             </Route>
           </Switch>
